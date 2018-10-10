51ed38ee91701c37479f3f379a4bd5b60a76e3b9
Alix Chagué <alix.chague@inria.fr>
2018-10-10 16:04:40 +0200

    adding configuration file

---
+++ main/config.py
@@ -0,0 +1,19 @@
+# Transkribus username / nom d'utilisateur Transkribus
+# e.g. - username = "username@mail.com"
+username = ""
+
+# Transkribus password / mot de passe Transkribus
+# e.g. - password = "password"
+password = ""
+
+# names of targeted collection(s) / nom de(s) collection(s) ciblée(s)
+# e.g. - collectionnames = ["collectionA", "collectionB", "collectionC"]
+collectionnames = [""]
+
+# target status(es) / statut(s) ciblé(s)
+# values limited to : NEW, IN PROGRESS, DONE or FINAL / les valeurs sont limitées à : NEW, IN PROGRESS, DONE ou FINAL
+# e.g. - status = ["FINAL"] or status = ["DONE", "IN PROGRESS"]
+status = [""]
+
+
+
