36534db5e0c7751d2d7d8e19648849aad18c734a
Alix Chagué <alix.chague@inria.fr>
2018-10-16 09:44:37 +0200

    added control on success/error transformation msg

--- main/run.py
+++ main/run.py
@@ -272,6 +272,7 @@ else:
                                 if os.path.isdir(path_abs) is True:
                                     xslt_doc_l.append(item)
 
+                            errors = 0
                             for xslt_input in xslt_doc_l:
                                 xslt_output = os.path.join(coll_dir, "TEI - %s" % xslt_input)
                                 xslt_input = os.path.join(coll_dir, xslt_input)
@@ -279,7 +280,11 @@ else:
                                 xslt_output = "-o:" + xslt_output
                                 xslt_input = "-s:" + xslt_input
                                 subprocess.call(["java", "-jar", SAXON_JAR, xslt_input, xslt_output, PAGE2TEI], env=env)
-                        print("Successfully transformed exported PAGE XML files to TEI XML!")
+                                errors += 1
+                        if errors == 0:
+                            print("Successfully transformed exported PAGE XML files to TEI XML!")
+                        else:
+                            print("Encountered errors while transforming exported XML files to TEI XML!")
                     print("Files are stored in %s directory." % path_to_export_dir)
 
 
