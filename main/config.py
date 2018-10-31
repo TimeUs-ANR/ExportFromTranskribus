247edf9d18293cc041e0545d07653d2316bcbb50
charlesriondet <charles.riondet@inria.fr>
2018-10-31 15:20:34 +0100

    Add Possibility to download specific documents from a collection

--- main/config.py
+++ main/config.py
@@ -10,10 +10,11 @@ password = ""
 # e.g. - collections = ["collectionA", "collectionB", "collectionC"]
 collections = [""]
 
+#names of targeted documents of the selected collection
+# documents = ["35199", "36685"]
+documents = [""]
+
 # target status(es) / statut(s) ciblé(s)
 # values limited to : NEW, IN_PROGRESS, DONE or FINAL / les valeurs sont limitées à : NEW, IN_PROGRESS, DONE ou FINAL
 # e.g. - status = ["FINAL"] or status = ["DONE", "IN_PROGRESS"]
 status = [""]
-
-
-
