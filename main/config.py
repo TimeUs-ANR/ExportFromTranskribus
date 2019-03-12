e87c2ced2886604580eed73bf5c793c7459edc74
charlesriondet <charles.riondet@inria.fr>
2019-03-12 13:09:59 +0100

    Merge branch 'master' of /Users/criondet/Documents/OCR/ExportFromTranskribus with conflicts.

--- main/config.py
+++ main/config.py
@@ -1,20 +1,20 @@
 # Transkribus username / nom d'utilisateur Transkribus
 # e.g. - username = "username@mail.com"
-username = ""
+username = "charles.riondet@inria.fr"
 
 # Transkribus password / mot de passe Transkribus
 # e.g. - password = "password"
-password = ""
+password = "P0stM0npw"
 
 # names of targeted collection(s) / nom de(s) collection(s) ciblée(s)
 # e.g. - collections = ["collectionA", "collectionB", "collectionC"]
-collections = [""]
+collections = ["timeUS"]
 
-# names of targeted documents of the selected collection
-# e.g. - documents = ["35199", "36685"]
-documents = [""]
+#names of targeted documents of the selected collection
+# documents = ["35199", "36685"]
+documents = ["41459"]
 
 # target status(es) / statut(s) ciblé(s)
 # values limited to : NEW, IN_PROGRESS, DONE or FINAL / les valeurs sont limitées à : NEW, IN_PROGRESS, DONE ou FINAL
 # e.g. - status = ["FINAL"] or status = ["DONE", "IN_PROGRESS"]
-status = [""]
+status = ["DONE","FINAL"]
